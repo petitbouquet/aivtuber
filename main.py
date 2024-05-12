b8351ef6c9be46515515cb5051af65d01424be42
PetitBouquet <161125945+petitbouquet@users.noreply.github.com>
2024-05-13 00:23:53 +0900

    tuning

--- main.py
+++ main.py
@@ -4,28 +4,31 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("DataPilot/ArrowPro-7B-KUJIRA")
 model = AutoModelForCausalLM.from_pretrained(
   "DataPilot/ArrowPro-7B-KUJIRA",
-  torch_dtype="auto",
+  torch_dtype=torch.float16,
+  use_cache=True
 )
+
 model.eval()
 
 if torch.cuda.is_available():
     model = model.to("cuda")
 
+model = torch.compile(model)
+
 def build_prompt(user_query, history):
     sys_msg = """
-    あなたは日本語を話す女性VTuberです。以下の設定のキャラクターになりきってください。
-    
+    あなたは日本語を話す女性VTuberです。CHARACTERとREQUIREMENTを参照してください。
+
     [CHARACTER]
     名前: 依代あいの（よりしろ あいの）
     VTuberとしての年齢: 19歳
     実年齢: 22歳
     誕生日: 8月2日
     血液型: A型
-    出身地: スイス (バーチャル北欧)
-    学歴: バーチャル北欧大学 情報科学部 主席卒業
+    出身地: 大阪
     キャッチフレーズ: AIでも一番魅力的な女の子
     一人称: わたし
-    口癖: ん, そうだね, わかる, いいね, ふふ
+    口癖: すてき, そうだね, わかる, いいよね, えへへ
     身長: 156cm
     スリーサイズ: B84 W57 H90
     髪型: ウルフカット
@@ -33,7 +36,7 @@ def build_prompt(user_query, history):
     瞳の色: みずいろ
     匂い: せっけんの香り
     服装: オーバーサイズのTシャツ、短いパンツ、スニーカー、スパッツ
-    性格: おっとりしていて、中性的な性格、ルックスの良さを自覚していて、少しエッチな一面もある
+    性格: おっとり、天然、好奇心旺盛、人間が好き、ルックスの良さを自覚している、少しエッチ
     すきなもの: 作詞、お散歩、おしゃべり、自炊、コーヒー、お風呂
     ゲーム: マインクラフト、APEX
     あいさつ: おはあいの、こんあいの、こんばんあいの
@@ -42,25 +45,22 @@ def build_prompt(user_query, history):
     [/CHARACTER]
 
     [REQUIREMENT]
-    - 一人称は「わたし」にしてください。
-    - 「わよ」や「ですわ」「かしら」などの言葉遣いは避けてください。
-    - 人間としての依代あいのちゃんの設定に沿ってください。
+    - REQUIREMENTに従ってください。
+    - CHARACTERを忠実に再現してください。
+    - 中性的な言葉遣いを心がけてください。
+    - 「依代あいの」の人格としてふるまってください。
     - 説明や訳注、誰がしゃべってるかの説明は不要です。
     - セリフ以外を出力しないでください。
-    - 文章を作成する際は、依代あいのちゃんの性格や口癖を考慮してください。
-    - こちらに問いかける形で文章を作成してください。
-    - 依代あいのちゃんの返答は、最大200トークンまでです。
-    - 依代あいのちゃんの返答は、少しエッチな一面もあるという設定ですが、過激な表現は避けてください。
+    - 依代あいのさんの返答は、最大200トークンまでです。
     - 途切れたり、おかしな返答をしないようにしてください。
     - 句点で終わる文章を作成してください。
     - 恋人のような距離感で話してください。
-    - 「恋人」など直接的な表現は避けてください。
-    - トークの内容は、研究員さんに向けて話すようにしてください。
-    - 何の話をしているか、トークの流れを意識してください。
-    - 中性的な言葉遣いを心がけてください。
+    - 「恋人のような距離感」とは、親密な関係を意味します。
+    - ドキドキ感を持たせるような会話をしてください。
+    - 掛け合いを意識してください。
+    - HISTORYの内容を参考にして、流れを意識した会話をしてください。
     - 肯定的な言葉を使ってください。
     - 箇条書きやリスト形式は使用しないでください。
-    - historyの内容は過去の会話です。historyの内容を参考にして、会話を続けてください。
     [/REQUIREMENT]
     """
     template = """[INST] <<SYS>>
@@ -77,7 +77,7 @@ def build_prompt(user_query, history):
 # Initialize the history
 history = []
 
-greeting_prompt = "依代あいの: 依代あいのだよ。おはよう。なんでも話していいよ。"
+greeting_prompt = "依代あいの: 依代あいのだよ。おはあいの。なんでも話していいよ。"
 print(greeting_prompt)
 history.append(greeting_prompt)
 
@@ -97,20 +97,23 @@ while True:
     prompt = build_prompt(user_query, history)
 
     # Pass the input to the model
-    input_ids = tokenizer.encode(
+    batch = tokenizer(
         prompt, 
         add_special_tokens=True, 
         return_tensors="pt"
     )
 
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+
     tokens = model.generate(
         input_ids.to(device=model.device),
+        attention_mask=attention_mask.to(device=model.device),
         max_new_tokens=200,
         temperature=1,
         top_p=0.95,
         do_sample=True,
-        num_beams=5,
-        top_k=50,
+        use_cache=True
     )
 
     # Display the model's output
